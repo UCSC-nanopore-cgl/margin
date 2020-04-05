42bf46d1b1756aa7463f88aad11bf4490f4bd530
benedictpaten <benedictpaten@gmail.com>
2020-04-05 11:39:00 -0700

    Fixing testing scripts
    Former-commit-id: 557ed4d66a869fef4a9ad98ca475d75f29e18f29

--- scripts/phaseTest.sh
+++ scripts/phaseTest.sh
@@ -9,7 +9,7 @@ set -o xtrace
 
 # Run margin
 echo Running Margin
-time ./margin ../tests/data/diploidTestExamples/${region}/*.bam ../tests/data/diploidTestExamples/${region}/HG002.shasta.*.fasta ../params/allParams.np.json ${options}  --logLevel DEBUG --diploid
+time ./margin ../tests/data/diploidTestExamples/${region}/*.bam ../tests/data/diploidTestExamples/${region}/HG002.shasta.*.fasta ../params/allParams.np.json ${options} --logLevel DEBUG --diploid
 
 # Calculate identity
 echo Comparing predicated haplotype1 to true haplotype1
@@ -25,11 +25,9 @@ echo Comparing predicated haplotype2 to true haplotype2
 time python3 ../scripts/dirty_assembly_compare.py ../tests/data/diploidTestExamples/${region}/HG002_h2*.fa output.fa.hap2
 
 # Build collection of differences
-time python3 ../scripts/dirty_assembly_compare.py output.fa.hap1 output.fa.hap2 verbose > predictedMismatches.txt
-time python3 ../scripts/dirty_assembly_compare.py ../tests/data/diploidTestExamples/${region}/HG002_h1*.fa ../tests/data/diploidTestExamples/${region}/HG002_h2*.fa verbose > trueMismatches.txt
+time python3 ../scripts/dirty_assembly_compare.py output.fa.hap1 output.fa.hap2 verbose >predictedMismatches.txt
+time python3 ../scripts/dirty_assembly_compare.py ../tests/data/diploidTestExamples/${region}/HG002_h1*.fa ../tests/data/diploidTestExamples/${region}/HG002_h2*.fa verbose >trueMismatches.txt
 
 # Compare differences
 echo Comparing predicted hets
 time python3 ../scripts/compareHets.py trueMismatches.txt predictedMismatches.txt
-
-
--- scripts/unphaseTest.sh
+++ scripts/unphaseTest.sh
@@ -9,7 +9,7 @@ set -o xtrace
 
 # Run margin
 echo Running Margin
-time ./margin ../tests/data/diploidTestExamples/${region}/*.bam ../tests/data/diploidTestExamples/${region}/HG002.shasta.*.fasta ../params/allParams.np.json ${options}  --logLevel DEBUG
+time ./margin ../tests/data/diploidTestExamples/${region}/*.bam ../tests/data/diploidTestExamples/${region}/HG002.shasta.*.fasta ../params/allParams.np.json ${options} --logLevel DEBUG
 
 # Calculate identity
 echo Comparing predicated sequence to true haplotype1
@@ -17,4 +17,3 @@ time python3 ../scripts/dirty_assembly_compare.py ../tests/data/diploidTestExamp
 
 echo Comparing predicated sequence to true haplotype2
 time python3 ../scripts/dirty_assembly_compare.py ../tests/data/diploidTestExamples/${region}/HG002_h2*.fa output.fa
-
