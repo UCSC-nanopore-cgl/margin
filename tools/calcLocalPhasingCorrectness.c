9854444ca64c30ab191cfbce8f1f76dfb138bb98
Trevor Pesout <tpesout@ucsc.edu>
2020-12-22 17:47:56 -0800

    fixing header row again

--- tools/calcLocalPhasingCorrectness.c
+++ tools/calcLocalPhasingCorrectness.c
@@ -284,7 +284,7 @@ int main(int argc, char *argv[]) {
     if (!bySeqDist) {
         printf("approx_");
     }
-    printf("length_scale_bps\t");
+    printf("length_scale_bps");
 
     if (!perVariant) {
         // show results aggregated across contigs
